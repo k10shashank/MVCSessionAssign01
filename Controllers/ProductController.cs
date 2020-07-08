ec3be9539112f8b825135a7d774caaefbd00953c
Shashank Keshari <vns.shashank10@gmail.com>
2020-07-08 22:45:26 +0530

    Commit on 2020 July 8

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCSessionAssign01.Models;
+
+namespace MVCSessionAssign01.Controllers
+{
+    public class ProductController : Controller
+    {
+        public static List<Product> productList = new List<Product>()
+        {
+            new Product(){ Pid = "P001", Pname = "Colgate", Price = 35, Stock = 100},
+            new Product(){ Pid = "P002", Pname = "Pepsodent", Price = 40, Stock = 150},
+            new Product(){ Pid = "P003", Pname = "CloseUp", Price = 30, Stock = 75},
+        };
+        
+        public IActionResult Index()
+        {
+            return View(productList);
+        }
+        
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product prod)
+        {
+            productList.Add(prod);
+            return RedirectToAction("Index","Product");
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCSessionAssign01.Models
+{
+    public class Product
+    {
+        public string Pid { get; set; }
+        public string Pname { get; set; }
+        public int Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
